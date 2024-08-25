554b7fb0ffafb40c8c797f3096521a5f3bedb3fa
Daniel Keane <keane.dp@gmail.com>
2024-08-25 17:51:47 -0500

    fly

--- scenes/plane/plane.gd
+++ scenes/plane/plane.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 
 const GRAVITY: float = 600.0
+const POWER: float = -200.0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,6 +12,15 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	print(velocity)
-	velocity.y += GRAVITY * delta;
+	fall(delta)
+	fly()
 	move_and_slide()
+
+
+func fall(delta: float) -> void:
+	velocity.y += GRAVITY * delta;
+
+
+func fly() -> void:
+	if Input.is_action_just_pressed(" fly"):
+		velocity.y = POWER
