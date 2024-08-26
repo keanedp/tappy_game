c4cb9bce456e9421492b5f73f7b9908e3a08e16c
Daniel Keane <keane.dp@gmail.com>
2024-08-26 05:36:30 -0500

    move pipes

---
+++ scenes/pipes/pipes.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+const SCROLL_SPEED: float = 120.0
+
+func _process(delta: float) -> void:
+	position.x -= SCROLL_SPEED * delta
+	check_offscreen()
+
+
+func check_offscreen() -> void:
+	if position.x < -500.0:
+		queue_free()
+
+
+func _on_screen_exited() -> void:
+	print("pipe exited screen")
+	set_process(false)
+	queue_free()
